6e9dd01a93a3013c1fe9cc87520174c3013866ee
Hui-Yu Huang <purelyvivid@gmail.com>
2017-12-29 12:29:02 +0800

    Update env_set_up.sh

--- env_set_up.sh
+++ env_set_up.sh
@@ -5,16 +5,18 @@
 # sh env_set_up.sh
 
 sudo apt-get update
-sudo apt purge nvidia-*
+#sudo apt purge nvidia-*
 sudo apt install nvidia-375
 
 # install CUDA Toolkit v8.0
 # instructions from https://developer.nvidia.com/cuda-downloads (linux -> x86_64 -> Ubuntu -> 16.04 -> deb (network))
+# https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 CUDA_REPO_PKG="cuda-repo-ubuntu1604_8.0.61-1_amd64.deb"
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG}
 sudo dpkg -i ${CUDA_REPO_PKG}
 sudo apt-get update
 sudo apt-get -y install cuda
+# After reboot, use 'nvidia-smi' to check whether cuda is installed sucessfully
 
 # install cuDNN v6.0
 CUDNN_TAR_FILE="cudnn-8.0-linux-x64-v6.0.tgz"
