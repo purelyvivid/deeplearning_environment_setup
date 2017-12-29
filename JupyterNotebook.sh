f13964a906ca528ad16f25be4177defed2e9fa16
Hui-Yu Huang <purelyvivid@gmail.com>
2017-12-29 14:30:33 +0800

    Update JupyterNotebook.sh

--- JupyterNotebook.sh
+++ JupyterNotebook.sh
@@ -1,16 +1,19 @@
+#!/bin/bash
+
 
 # install Ipython:
 sudo apt-get -y install ipython ipython-notebook
 
 # install Jupyter Notebook
-sudo pip install --user jupyter
+sudo pip install --user jupyter #successful!!
+
 # below will has some issue: Command "python setup.py egg_info" failed with error code 1 in /tmp/pip-build-NG6zOV/unroll/
 #sudo -H pip install jupyter
 # below are not nessesary!!
-sudo pip install --upgrade pip
-sudo pip install --upgrade setuptools
-sudo pip install ez_setup
-sudo pip install unroll   # has error!!
-sudo easy_install -U setuptools
+#sudo pip install --upgrade pip
+#sudo pip install --upgrade setuptools
+#sudo pip install ez_setup
+#sudo pip install unroll   # has error!!
+#sudo easy_install -U setuptools
 
 
