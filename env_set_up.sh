e7ba9a1e755f857da3db70a66337e83f02896c5a
Hui-Yu Huang <purelyvivid@gmail.com>
2018-01-12 13:30:30 +0800

    Update env_set_up.sh

--- env_set_up.sh
+++ env_set_up.sh
@@ -17,11 +17,11 @@ sudo apt install nvidia-375
 # https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64.deb
 
 # below will install cuda 9.1 , don't know why... 
-#CUDA_REPO_PKG="cuda-repo-ubuntu1604_8.0.61-1_amd64.deb"
-#wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG}
-#sudo dpkg -i ${CUDA_REPO_PKG}
-#sudo apt-get update
-#sudo apt-get -y install cuda
+CUDA_REPO_PKG="cuda-repo-ubuntu1604_8.0.61-1_amd64.deb"
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG}
+sudo dpkg -i ${CUDA_REPO_PKG}
+sudo apt-get update
+sudo apt-get -y install cuda
 
 #below will install cuda 8.0
 sudo apt-get install cuda-8-0
