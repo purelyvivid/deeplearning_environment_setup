c5c73ce6a3376aab9df8b1cf84131fda837c3a01
Hui-Yu Huang <purelyvivid@gmail.com>
2018-01-12 13:34:53 +0800

    Update env_set_up.sh

--- env_set_up.sh
+++ env_set_up.sh
@@ -21,7 +21,7 @@ CUDA_REPO_PKG="cuda-repo-ubuntu1604_8.0.61-1_amd64.deb"
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG}
 sudo dpkg -i ${CUDA_REPO_PKG}
 sudo apt-get update
-sudo apt-get -y install cuda
+#sudo apt-get -y install cuda
 
 #below will install cuda 8.0
 sudo apt-get install cuda-8-0
