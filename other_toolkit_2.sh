6d57f1613fdaa00b0e6a4f5d572bfeaab42a227d
Hui-Yu Huang <purelyvivid@gmail.com>
2018-04-30 14:14:27 +0800

    add nodejs

--- other_toolkit_2.sh
+++ other_toolkit_2.sh
@@ -21,6 +21,24 @@ sudo apt-get install --no-install-recommends yarn
 yarn --version
 # 1.6.0
 
+# nodejs
+# https://blog.gtwang.org/web-development/install-node-js-in-windows-mac-os-x-linux/
+# 在 Linux 下安裝 Node.js - 下載 Node.js 二進位檔安裝
+# download binary file from offical website: https://nodejs.org/en/download/
+# 從 Node.js 的官方網站下載編譯好的二進位版本（Linux Binaries），解壓縮之後就可以使用了。
+# 接著把解壓縮出來的 node-v8.11.1-linux-x64 目錄放到想要安裝的位置就行了，常見的方式是把它放在 /opt 下面，就像這樣：
+fname=node-v8.11.1-linux-x64
+sudo mv ${fname} /opt/
+# 而 node 與 npm 這兩個指令就存放在其中的 bin/ 這個目錄中，加上絕對路徑即可執行，例如：
+/opt/${fname}/bin/node -h
+# 把這個路徑加入 PATH 中：
+export PATH=$PATH:/opt/${fname}
+# 如果要讓每次登入都會自動設定 PATH，可以把上面這行加在自己的 ~/.bashrc 中：
+echo 'PATH=$PATH:/opt/node-v8.11.1-linux-x64' >> ~/.bashrc
+
+
+
+
 
 
 
