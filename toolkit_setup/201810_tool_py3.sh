b47e3838017b6ce5676cb62715edb1669bde35fd
Hui-Yu Huang <purelyvivid@gmail.com>
2018-10-29 16:11:11 +0800

    Update 201810_tool_py3.sh

--- toolkit_setup/201810_tool_py3.sh
+++ toolkit_setup/201810_tool_py3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 """
+Chimei Formal Machine
 Evironment:
 Cuda-9.0 + CuDnn v7 + Tensorflow 1.11.0 + python3.5 on Ubuntu 16.04.4
 """
@@ -21,13 +22,12 @@ sudo apt install git #success
 IDE common used
 """
 #---Jupyter Notebook---
+# https://www.digitalocean.com/community/tutorials/how-to-set-up-a-jupyter-notebook-to-run-ipython-on-ubuntu-16-04
+
 # install Ipython:
-sudo apt-get -y install ipython ipython-notebook #successful
+sudo apt-get -y install ipython ipython-notebook
 # install Jupyter Notebook
-sudo pip3 install --user jupyter #successful
-sudo pip3 install jupyter   #successful
-# below 'aiuser' change to your own user name
-sudo chown aiuser:aiuser ~/.local/share/jupyter #successful
+sudo -H pip install jupyter
 
 #---Jupyter Lab---
 sudo pip3 install jupyterlab
@@ -42,8 +42,9 @@ sudo pip3 install numpy #successful #Requirement already satisfied
 sudo pip3 install pandas  #successful
 
 #---matplotlib---
-sudo python -mpip install -U pip #successful
-sudo python -mpip install -U matplotlib  #successful
+#sudo python -mpip install -U pip #successful
+#sudo python -mpip install -U matplotlib  #successful
+sudo pip3 install matplotlib
 
 #---sklearn---
 sudo pip3 install sklearn  #successful
@@ -65,9 +66,11 @@ sudo pip3 install torch torchvision
 markdown editor
 """
 #---typora---
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE # optional, but recommended
-sudo add-apt-repository 'deb https://typora.io ./linux/' # add Typora's repository
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+# add Typora's repository
+sudo add-apt-repository 'deb https://typora.io/linux ./'
 sudo apt-get update
+# install typora
 sudo apt-get install typora
 
 """
